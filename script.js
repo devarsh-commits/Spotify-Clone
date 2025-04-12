28c7c83ac9a1257997513091bed393356957f854
Devarsh Dubey <devarsh.d10@gmail.com>
2025-04-13 00:31:45 +0530

    ..

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Let's start our JavScript")
+let currentsong = new Audio();
 //Writing a program to add cards dynamically
 async function fetchsong(params) {
     let a = await fetch("http://127.0.0.1:3000/songs/");
@@ -18,31 +19,72 @@ async function fetchsong(params) {
 
 
 }
+function formatTime(seconds) {
+    seconds = Math.floor(seconds); // Remove milliseconds
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+
+    return `${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+}
+
+
+const playmusic = (track,pause=false) => {
+    // let audio = new Audio("/songs/" + track)
+    currentsong.src = "/songs/" + track;
+    if(!pause){
+        currentsong.play();
+        play.src="pause.svg";
+    }
+    document.querySelector(".songinfo").innerHTML=decodeURI(track);
+    document.querySelector(".songtime").innerHTML="00:00/00:00"
+}
 async function main(params) {
     //get the list of the songs
     let songs = await fetchsong();
+    playmusic(songs[0],true);
+
+
     //show all the songs in the playlists
     let songsol = document.querySelector(".playlist").getElementsByTagName("ol")[0]
     for (const song of songs) {
         songsol.innerHTML = songsol.innerHTML + `  <li>
                     <img src="music.svg" alt="" class="c-svg">
-                    <div>${song.replaceAll("_", " ")}</div>
+                    <div>${song.replaceAll("%20", " ")}</div>
                     <div class="wrap flex">Play Now <img src="splay.svg" alt="" class="c-svg"></div>
                 </li>`;
 
     }
-    // //play the first song code
-    // var audio = new Audio(songs[0]);
-    // // audio.play();
-    // audio.addEventListener("loadeddata", () => {
-    //     console.log(audio.duration, audio.src, audio.currentTime)
-    // })
-    Array.from(document.querySelector(".playlist").getElementsByTagName("li")).forEach(e=>{
-        console.log(e)
+    //play the first song code
+    //     var audio = new Audio(songs[0]);
+    //     audio.play();
+    //     audio.addEventListener("loadeddata", () => {
+    //    console.log(audio.duration, audio.src, audio.currentTime)
+    //     })
+    Array.from(document.querySelector(".playlist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click", element => {
+            console.log(e.getElementsByTagName("div")[0].innerHTML)
+            playmusic(e.getElementsByTagName("div")[0].innerHTML);
+        })
+    })
+    play.addEventListener("click",()=>{
+        if(currentsong.paused){
+            currentsong.play();
+            play.src="pause.svg"
+        }
+        else{
+            currentsong.pause()
+            play.src="splay.svg"
+        }
+    })
+
+    //listen for time update event
+    currentsong.addEventListener("timeupdate",()=>{
+        console.log(currentsong.currentTime,currentsong.duration);
+        document.querySelector(".songtime").innerHTML=`${formatTime(currentsong.currentTime)}:${formatTime(currentsong.duration)}`
     })
 }
 main();
-l
+
 
 
 
